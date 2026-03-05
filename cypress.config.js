339964d55f192e584d0ac36053ea8e2d3027c3f5
Gustavo Moreno <gustavo_moreno.182@hotmail.com>
2026-03-05 14:59:24 -0300

    add Cypress Cloud projectId na main

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "a37y81",
+
   e2e: {
-    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    baseUrl: "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
